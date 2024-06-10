835302b393914498fdc1158730e4de3f121f69ba
Petzlc <chlodwig.petzl@hotmail.com>
2024-06-10 17:45:12 +0100

    Add very basic styling to products

--- app/products/page.js
+++ app/products/page.js
@@ -1,7 +1,9 @@
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { getProducts } from '../../database/products';
-import styles from '../page.module.scss';
+import styles from './productsPage.module.scss';
+
+// import styles from '../page.module.scss';
 
 export const metadata = {
   title: 'Products',
@@ -12,20 +14,22 @@ export default function ProductsPage() {
   const products = getProducts();
   return (
     <div>
-      This are the products
-      {products.map((product) => {
-        return (
-          <div key={`products-${product.id}`}>
-            <div>{product.name}</div>
-            <Image
-              src={`/img/${product.name}.png`}
-              alt=""
-              width="53"
-              height="244"
-            />
-          </div>
-        );
-      })}
+      <h5>This are the products</h5>
+      <div className={styles.productsContainer}>
+        {products.map((product) => {
+          return (
+            <div className={styles.productBox} key={`products-${product.id}`}>
+              <div>{product.name}</div>
+              <Image
+                src={`/img/${product.name}.png`}
+                alt={`${product.name}`}
+                width="53"
+                height="244"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
 
     // <div>
