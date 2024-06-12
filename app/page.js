63fa36a928649a974a8742709d75e2021736348a
Petzlc <chlodwig.petzl@hotmail.com>
2024-06-12 15:18:01 +0100

    Add singleproducts page with picture

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import GenerateButton from './GenerateButton';
+import LocalStorage from './LocalStorage';
 
 export default function Home() {
   return (
     <div>
       <GenerateButton />
+      <LocalStorage />
       <h1>Home</h1>
     </div>
   );
--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,7 +1,19 @@
+import Image from 'next/image';
 import { getProduct } from '../../../database/products';
 
 export default function SingleProductPage(props) {
   const singleProduct = getProduct(Number(props.params.productId));
-  console.log('props: ', singleProduct);
-  return <div>SingleProductPage</div>;
+  return (
+    <div>
+      <h1>{singleProduct.name}</h1>
+      <div>
+        <Image
+          src={`/img/${singleProduct.name}.webp`}
+          alt={`${singleProduct.name}`}
+          width="53"
+          height="244"
+        />
+      </div>
+    </div>
+  );
 }
