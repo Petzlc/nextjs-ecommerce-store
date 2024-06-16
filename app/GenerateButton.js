c166a7b4cedc912a4d1000a26e7d9505b40ca876
Petzlc <chlodwig.petzl@hotmail.com>
2024-06-16 17:13:46 +0100

    Add cookies for generate button

--- app/GenerateButton.js
+++ app/GenerateButton.js
@@ -1,29 +1,38 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './GenerateButton.module.scss';
 
 export default function GenerateButton() {
-  const [color, setColor] = useState('#123fee');
+  const [color, setColor] = useState('');
   const router = useRouter();
 
   // document.cookie (don't use it, it's an antipatern. Just to showcase)
-  const allCookies = document.cookie;
+  useEffect(() => {
+    // DON'T USE document.cookie
+    // DON'T USE document.cookie (because of cross-side-script Attack. We use an API instead)
 
-  const cookieValue = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith('test2='))
-    ?.split('=')[1];
+    const buttonColor = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('buttonColor='))
+      ?.split('=')[1];
 
-  console.log(cookieValue);
-
+    setColor(
+      buttonColor || `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+    );
+  }, []);
   return (
     <button
       className={styles.generateButton}
       style={{ backgroundColor: color }}
       onClick={() => {
-        setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+        const newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; // For the cookie
+
+        document.cookie = `buttonColor=${newColor}`;
+
+        setColor(newColor);
+        // setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
         router.refresh();
       }}
     >
