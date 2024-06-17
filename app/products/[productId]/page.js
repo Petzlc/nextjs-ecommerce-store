6a9f97f7d6a322b9a0e6d378e13f7fd25d5b3bdc
Petzlc <chlodwig.petzl@hotmail.com>
2024-06-17 13:43:19 +0100

    Add not-found page

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 import { getProductInsecure } from '../../../database/products'; // was without Insecure first
 import QuantityButton from '../../QuantityButton';
 
@@ -8,6 +9,9 @@ export default async function SingleProductPage(props) {
   const singleProduct = await getProductInsecure(
     Number(props.params.productId),
   );
+  if (!singleProduct) {
+    notFound();
+  }
 
   // const for individual style for the single products. it does not work since i can somehow not overwrite the height and width of the <image> element
   // const imageClass = styles[`image${singleProduct.name}`];
